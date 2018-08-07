a53f9e7519fa2ec72f34bf9626465aba50c9a057
Octowl <alsaff1987@gmail.com>
2018-08-07 14:44:13 +0300

    Add books link to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,6 +12,9 @@ function Sidebar() {
         <h4 className="menu-item">
           <NavLink to="/authors">AUTHORS</NavLink>
         </h4>
+        <h4 className="menu-item">
+          <NavLink to="/books">BOOKS</NavLink>
+        </h4>
       </section>
     </div>
   );
