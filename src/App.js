8641bc4e66bf178d8e24119a4fb7eb89f8821a27
Octowl <alsaff1987@gmail.com>
2018-08-09 15:20:02 +0300

    Add optional bookColor parameter to books path

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
             )}
           />
           <Route
-            path="/books/"
+            path="/books/:bookColor?"
             render={props => <BookList {...props} books={this.state.books} />}
           />
         </Switch>
