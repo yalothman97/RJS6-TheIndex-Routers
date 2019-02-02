6978244925d6309fbff04e5725afc0672ce27467
Octowl <alsaff1987@gmail.com>
2019-02-02 19:18:02 +0300

    Fix syntax

--- src/BookList.js
+++ src/BookList.js
@@ -1,51 +1,38 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 // Components
 import SearchBar from "./SearchBar";
 import BookTable from "./BookTable";
 
 class BookList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filteredBooks: this.props.books
-    };
+  state = {
+    filteredBooks: this.props.books
+  };
 
-    this.filterBooks = this.filterBooks.bind(this);
-  }
-
-  filterBooks(query) {
+  filterBooks = query => {
     query = query.toLowerCase();
     let filteredBooks = this.props.books.filter(book =>
       book.title.toLowerCase().includes(query)
     );
     this.setState({ filteredBooks });
-  }
+  };
 
-  filterBooksByColor(bookColor) {
+  filterBooksByColor = bookColor => {
     return this.state.filteredBooks.filter(book => book.color === bookColor);
-  }
+  };
 
   render() {
     const bookColor = this.props.match.params.bookColor;
     let books = this.state.filteredBooks;
-    let allBooksButton;
 
     if (bookColor) {
       books = this.filterBooksByColor(bookColor);
-      allBooksButton = (
-        <Link to="/books">
-          <button className="btn">All Books</button>
-        </Link>
-      );
     }
 
     return (
       <div>
         <h3>Books</h3>
-        <SearchBar changeHandler={this.filterBooks} />
-        {allBooksButton}
+        <SearchBar onChange={this.filterBooks} />
         <BookTable books={books} />
       </div>
     );
