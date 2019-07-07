e367ef2b3813fb9409127d8069818f53c0877fe4
Lailz <lailaabdulraheem@gmail.com>
2019-07-07 18:42:14 +0300

    update react

--- src/BookRow.js
+++ src/BookRow.js
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-class BookRow extends Component {
-  render() {
-    const book = this.props.book;
-    return (
-      <tr>
-        <td>{book.title}</td>
-        <td>
-          {book.authors.map(author => (
-            <div key={author.id}>{author.name}</div>
-          ))}
-        </td>
-        <td>
-          <button className="btn" style={{ backgroundColor: book.color }} />
-        </td>
-      </tr>
-    );
-  }
+function BookRow(props) {
+  const book = props.book;
+  const authors = book.authors.map(author => (
+    <div key={author.id}>{author.name}</div>
+  ));
+  return (
+    <tr>
+      <td>{book.title}</td>
+      <td>{authors}</td>
+      <td>
+        <button className="btn" style={{ backgroundColor: book.color }} />
+      </td>
+    </tr>
+  );
 }
 
 export default BookRow;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 // Logo
 import logo from "./assets/theindex.svg";
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <div id="sidebar">
-        <img src={logo} className="logo" alt="the index logo" />
-        <section>
-          <h4 className="menu-item">
-            <NavLink to="/authors">AUTHORS</NavLink>
-          </h4>
-        </section>
-      </div>
-    );
-  }
+function Sidebar() {
+  return (
+    <div id="sidebar">
+      <img src={logo} className="logo" alt="the index logo" />
+      <section>
+        <h4 className="menu-item">
+          <NavLink to="/authors">AUTHORS</NavLink>
+        </h4>
+      </section>
+    </div>
+  );
 }
 
 export default Sidebar;
